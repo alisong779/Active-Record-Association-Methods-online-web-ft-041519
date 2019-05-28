3012e72cd66927ce77e10f01523e6c9d48b950b9
Alison Golden <alisong779@yahoo.com>
2019-05-28 19:51:50 +0000

    Automatically backed up by Learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -15,6 +15,7 @@ class Genre < ActiveRecord::Base
   def all_artist_names
     artists = self.artists.collect do |artist|
       artist.name
+    end 
     # return an array of strings containing every musician's name
   end
 end
